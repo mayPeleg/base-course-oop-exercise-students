9295e53136c67048f9790f06f6c017a72ee3caf4
may <maypeleg00@gmail.com>
2020-06-08 21:02:04 +0300

    change Coordinates class

--- src/main/java/AerialVehicles/FightAircraft.java
+++ src/main/java/AerialVehicles/FightAircraft.java
@@ -5,7 +5,7 @@ import Missions.Mission;
 public class FightAircraft extends AerialVehicle implements AerialAttackVehicle{
     public int missiles;
     public String missileType;
-    public final int MAX_HOURS_OF_FLIGHT_UNTIL_REPAIR = 250;
+    public final int MAX_HOURS_OF_FLIGHT_UNTIL_REPRAIR = 250;
 
     FightAircraft(String pilotName, Mission mission, int hoursOfFlightSinceLastRepair, boolean flightStatus, int missiles, String missileType) {
         super(pilotName, mission, hoursOfFlightSinceLastRepair, flightStatus, 250);
